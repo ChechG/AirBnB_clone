982b1663076e714d9c01bb10f05b22aa8f943fab
ChechG <2100@holbertonschool.com>
2021-02-18 16:33:04 +0000

    tutti

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" Tests for Amenity class """
+import unittest
+import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.amenity import Amenity
+import models
+
+
+class TestingAmenity(unittest.TestCase):
+    """ Amenity class - tests """
+    def test_User1(self):
+        """ Test of the Amenity class """
+        my_amenity1 = User()
+
+    def test_Amenity2(self):
+        """ Test of the Amenity class """
+        my_amenity2 = Amenity(1)
+        my_amenity3 = Amenity("hola")
+        my_amenity4 = Amenity([1, 2, 3])
+        my_amenity5 = Amenity({"hola": "chau"})
+
+    def test_Amenity3(self):
+        """ Test of the Amenity class """
+        self.assertEqual(str(type(Amenity)), "<class 'type'>")
+
+    def test_Amenity4(self):
+        """ Test of the Amenity class """
+        my_amenity6 = Amenity()
+        self.assertEqual(isinstance(my_amenity6, Amenity), True)
+
+    def test_Amenity5(self):
+        """ Test of the Amenity class """
+        my_amenity7 = Amenity()
+        self.assertEqual(issubclass(Amenity, BaseModel), True)
+
+    def test_Amenity6(self):
+        """ Test of the Amenity class """
+        self.assertEqual(issubclass(Amenity, FileStorage), False)
+
+    def test_Amenity7(self):
+        """ Test of the Amenity class """
+        my_amenity8 = Amenity()
+        my_amenity8.name = "Andy"
+        self.assertEqual(str(type(my_amenity8.name)), "<class 'str'>")
+
+    def test_Amenity8(self):
+        """ Test of the Amenity class """
+        my_amenity9 = Amenity()
+        my_amenity9.name = "Gini"
+        self.assertTrue("name" in my_amenity9.__dict__)
+
+    def test_Amenity9(self):
+        """ Test of the Amenity class """
+        my_amenity10 = Amenity()
+        self.assertFalse("first" in my_amenity10.__dict__)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Tests for City class """
+import unittest
+import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.city import City
+import models
+
+
+class TestingCity(unittest.TestCase):
+    """ City class - tests """
+    def test_City1(self):
+        """ Test of the City class """
+        my_City1 = City()
+
+    def test_City2(self):
+        """ Test of the City class """
+        my_City2 = City(1)
+        my_City3 = City("hola")
+        my_City4 = City([1, 2, 3])
+        my_City5 = City({"hola": "chau"})
+
+    def test_City3(self):
+        """ Test of the City class """
+        self.assertEqual(str(type(City)), "<class 'type'>")
+
+    def test_City4(self):
+        """ Test of the City class """
+        my_City6 = City()
+        self.assertEqual(isinstance(my_City6, City), True)
+
+    def test_City5(self):
+        """ Test of the City class """
+        my_City7 = City()
+        self.assertEqual(issubclass(City, BaseModel), True)
+
+    def test_City6(self):
+        """ Test of the City class """
+        self.assertEqual(issubclass(City, FileStorage), False)
+
+    def test_City7(self):
+        """ Test of the City class """
+        my_City8 = City()
+        my_City8.state_id = "Colorado"
+        my_City8.name = "Denver"
+        self.assertEqual(str(type(my_City8.state_id)), "<class 'str'>")
+        self.assertEqual(my_City8.state_id, "Colorado")
+        self.assertEqual(str(type(my_City8.name)), "<class 'str'>")
+
+    def test_City8(self):
+        """ Test of the City class """
+        my_City9 = City()
+        my_City9.state_id = "Borussia"
+        my_City9.name = "Dortmund"
+        self.assertTrue("state_id" in my_City9.__dict__)
+        self.assertTrue("name" in my_City9.__dict__)
+
+    def test_City9(self):
+        """ Test of the City class """
+        my_City10 = City()
+        self.assertFalse("first" in my_City10.__dict__)
+
+if __name__ == "__main__":
+    unittest.main()
