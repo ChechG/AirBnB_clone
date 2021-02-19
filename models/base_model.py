91be4102b3f43560b2d9b293176bd8435510301a
Tadeo Grach <tade.g.17@gmail.com>
2021-02-19 00:02:34 +0000

    re-adding basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from models import storage
 
 
 class BaseModel:
-    """Defines all common attr/methods for other classes"""
+    """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
         """Initializes object"""
@@ -48,4 +48,3 @@ class BaseModel:
             else:
                 dict_returned[key] = value
         return (dict_returned)
-
